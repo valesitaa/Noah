fceb3d7894603dd06fa3575b8165e08b3ebb87bb
valesitaa <jacyaravalentina.icccp@gmail.com>
2023-03-22 12:28:53 -0300

    megusta p1

---
+++ Megusta/megusta.js
@@ -0,0 +1,16 @@
+var count = 3;
+var countElement = document.querySelector("#count");
+console.log(countElement);
+
+function addShadow(element) {
+    element.classList.add("shadow")
+}
+function removeShadow(element) {
+    element.classList.remove("shadow")
+}
+     function likes() {
+        count++;
+        countElement.innerText = count + "like(s)";
+        console.log(count)
+     }
+    
