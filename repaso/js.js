0e291092681e70d3d15a0e9de5d3690d4eb2de52
valesitaa <jacyaravalentina.icccp@gmail.com>
2023-06-02 14:40:49 -0400

    subiendo contenido

---
+++ repaso/js.js
@@ -0,0 +1,45 @@
+var name = "Marisa";
+var message = "JS es, de hecho, el lenguaje más complicado";
+console.log(name + " says " + message);
+
+
+var name = prompt("Ingresa tú Nombre");
+var name2 = prompt("Ingresa tú Apellido");
+var count = prompt("Ingresa tú edad");
+
+console.log(name + name2 + count);
+
+var v = prompt("Ingresa un numero");
+var x = v/3;
+console.log(x)
+var y = x+10;
+console.log(y)
+var j = y*2;
+console.log(j)
+
+
+
+var v = prompt("Ingresa un numero");
+var m = prompt("Ingresa un numero");
+if(v > m) {
+    console.log("creciente")
+}
+else{
+    console.log("decreciente")
+}
+
+
+
+var v = parseInt(prompt("Ingresa un numero"));
+var j = parseInt(prompt("Ingresa un numero"));
+var m = parseInt(prompt("Ingresa un numero"));
+var c = parseInt(prompt("Ingresa un numero"));
+var n = parseInt(prompt("Ingresa un numero"));
+var e = (v+j+m+c+n)/5;
+console.log(e);
+
+
+
+
+
+var m = parseInt(prompt("Ingresa un numero"));
